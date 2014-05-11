04285d0751ef6f5454611de48bd26c5aed08ca04
Matthew Andrews <matt@mattandre.ws>
2014-05-11 11:34:19 +0100

    Two-way communication

--- worker.js
+++ worker.js
@@ -3,13 +3,15 @@ this.version = 1;
 
 this.addEventListener('install', function(e) {
   console.log("I am a Service Worker and I have been installed");
-});
+}.bind(this));
 
 this.addEventListener('activate', function(e) {
   console.log("I am a Service Worker and I have been activated");
-});
+}.bind(this));
 
 this.addEventListener('message', function(e) {
   var message = e.data;
-  console.log("I am a Service Worker and I have just got the message:"+message);
-});
+  console.log("Service Worker just got a message");
+  console.log(message.text);
+  message.port.postMessage("We got your message: "+message.text);
+}.bind(this));
