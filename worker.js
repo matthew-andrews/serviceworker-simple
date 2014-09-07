f2f7469636ec8ea55cf8ce4e36cae8563bcfa679
Matt Andrews <matt@mattandre.ws>
2014-09-07 15:20:12 +0100

    There really is no need for binding

--- worker.js
+++ worker.js
@@ -3,17 +3,8 @@ this.version = 1;
 
 this.addEventListener('install', function(e) {
   console.log("I am a Service Worker and I have been installed");
-}.bind(this));
+});
 
 this.addEventListener('activate', function(e) {
   console.log("I am a Service Worker and I have been activated");
-}.bind(this));
-
-this.addEventListener('message', function(e) {
-  var message = e.data;
-  console.log("Service Worker just got a message");
-  console.log(message.text);
-  setTimeout(function() {
-    message.port.postMessage("We got your message: "+message.text);
-  }, 2500);
-}.bind(this));
+});
